c13d554b53e69f5f1ea6254e9329cd3b6ea79316
Arthurpds1 <arthurcml20@gmail.com>
2026-04-30 15:47:53 -0300

    Terceiro commit

--- produção_derivados.py
+++ produção_derivados.py
@@ -6,7 +6,7 @@ produtos = []
 
 while True:
 
-    op = input('------O que deseja fazer?------ \n 1-Registrar produção de leite \n 2-Atualizar alimentos \n 0-retornar ao menu \n')
+    op = input('\n------O que deseja fazer?------\n 1-Registrar produção de leite \n 2-Atualizar alimentos \n 3-Verificar produção \n 4-Verificar Estoque \n 0-retornar ao menu \n')
 
     if op == '1':
         dia = input('Digite o dia atual: ')
@@ -19,12 +19,12 @@ while True:
     
     elif op == '2':
         while True:
-            att_alimentos = input('--------O que deseja atualizar?------ \n 1-Registrar Alimento \n 2-remover produto \n 3-Atualizar preços \n 0-Cancelar')
+            att_alimentos = input('\n--------O que deseja atualizar?------\n 1-Registrar Alimento \n 2-remover produto \n 3-Atualizar preços \n 0-Cancelar \n')
             
             if att_alimentos == '1':
-                produto = input('Qual produto deseja adicionar?')
-                peso = float(input('Qual o peso do produto?'))
-                preço = float(input('Qual o preço do produto?'))
+                produto = input('Qual produto deseja adicionar? ')
+                peso = float(input('Qual o peso do produto? '))
+                preço = float(input('Qual o preço do produto? '))
 
                 alimento = [produto, peso, preço]
                 produtos.append(alimento)
@@ -57,11 +57,29 @@ while True:
                         print('Alimento Atualizado.')
                         encontrado = 1
                     
-                    if encontrado == 0:
-                        print('Alimento não encontrado.')
+                if encontrado == 0:
+                    print('Alimento não encontrado.')
 
             elif att_alimentos == '0':
                 break
+    elif op == '3':
+        print('\n------HISTORICO DE PRODUÇÃO------\n')
+
+        if len(producao_leite) == 0:
+            print('Nenhuma produção cadastrada.')
+        else:
+            for leite in producao_leite:
+                print('Dia:', leite[0], '| Litros:', leite[1])
+    
+    elif op == '4':
+        print('------ESTOQUE DE PRODUTOS------ \n')
+        if len(produtos) == 0:
+            print('Nenhum produto cadastrado.')
+        else:
+            for prod in produtos:
+                print('Produto:', prod[0],
+                    '| Quantidade:', prod[1],
+                    '| Preço:', prod[2])
     
     elif op == '0':
         break
