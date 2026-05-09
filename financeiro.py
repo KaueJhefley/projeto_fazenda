e764870e97307df33562f0aa96127af65433b3de
KaueJhefley <kauejhefley@gmail.com>
2026-05-09 13:45:51 -0300

    financeiro

--- financeiro.py
+++ financeiro.py
@@ -1,1 +1,33 @@
-dinheiro = []
+receitas = []
+despesas = []
+saldo = 0
+
+
+while True:
+    financeiro = input('----Gerenciamento Financeiro---- \n 1-registrar receita \n 2-registrar despesa \n 3-ver saldo\n 4-relatorio financeiro\n 0-voltar\n')
+    if financeiro == '1':
+        descricao = input('Digite a descrição da receita: ')
+        valor = float(input('Digite o valor da receita: '))
+        data = input('Digite a data da receita (DD/MM/AAAA): ')
+        receitas.append([descricao, valor, data])
+        saldo += valor
+        print('Receita registrada com sucesso.')
+    elif financeiro == '2':
+        descricao = input('Digite a descrição da despesa: ')
+        valor = float(input('Digite o valor da despesa: '))
+        data = input('Digite a data da despesa (DD/MM/AAAA): ')
+        despesas.append([descricao, valor, data])
+        saldo -= valor
+        print('Despesa registrada com sucesso.')
+    elif financeiro == '3':
+        print('saldo atual: R$', saldo)
+    elif financeiro == '4':
+        print('----RELATÓRIO FINANCEIRO----')
+        print('----Receitas----')
+        for r in receitas:
+            print(f'{r[0]} - R$ {r[1]} - {r[2]}')
+            print('----Despesas----')
+        for d in despesas:
+            print(f'{d[0]} - R$ {d[1]} - {d[2]}')
+    elif financeiro == '0':
+        break
--- menu.py
+++ menu.py
@@ -12,9 +12,10 @@ producao_leite = []
 producao_derivados = []
 leite_disponivel = 0
 derivados = []
-
 produtos = []
-
+receitas = []
+despesas = []
+saldo = 0
 op = -99
 index = -99
 menu = -99
@@ -520,6 +521,35 @@ while menu != 0:
                                     print() 
                             
                             elif op_produçao == '0':
+                                break
+                    elif menu_adm == '3':
+                        while True:
+                            financeiro = input('----Gerenciamento Financeiro---- \n 1-registrar receita \n 2-registrar despesa \n 3-ver saldo\n 4-relatorio financeiro\n 0-voltar\n')
+                            if financeiro == '1':
+                                descricao = input('Digite a descrição da receita: ')
+                                valor = float(input('Digite o valor da receita: '))
+                                data = input('Digite a data da receita (DD/MM/AAAA): ')
+                                receitas.append([descricao, valor, data])
+                                saldo += valor
+                                print('Receita registrada com sucesso.')
+                            elif financeiro == '2':
+                                descricao = input('Digite a descrição da despesa: ')
+                                valor = float(input('Digite o valor da despesa: '))
+                                data = input('Digite a data da despesa (DD/MM/AAAA): ')
+                                despesas.append([descricao, valor, data])
+                                saldo -= valor
+                                print('Despesa registrada com sucesso.')
+                            elif financeiro == '3':
+                                print('saldo atual: R$', saldo)
+                            elif financeiro == '4':
+                                print('----RELATÓRIO FINANCEIRO----')
+                                print('----Receitas----')
+                                for r in receitas:
+                                    print(f'{r[0]} - R$ {r[1]} - {r[2]}')
+                                print('----Despesas----')
+                                for d in despesas:
+                                    print(f'{d[0]} - R$ {d[1]} - {d[2]}')
+                            elif financeiro == '0':
                                 break
             else:
                 print('senha invalida')
